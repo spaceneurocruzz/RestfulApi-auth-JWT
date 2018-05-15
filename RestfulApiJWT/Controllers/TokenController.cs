f2536844d2aa7509fc336af60404aac9bedf6e47
Sandy Lin <hjsandy_lin@acbel.com>
2018-05-15 23:16:32 +0800

    create api interface and models

---
+++ RestfulApiJWT/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RestfulApiJWT.Models;
+
+namespace RestfulApiJWT.Controllers
+{
+	[Route("api/[controller]")]
+	public class TokenController : Controller
+	{
+		[HttpPost]
+		public IActionResult CreateToken([FromBody]LoginModel login)
+		{
+			
+		}
+
+		private string BuildToken(UserModel user)
+		{
+		
+		}
+
+	}
+}
---
+++ RestfulApiJWT/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulApiJWT.Models
+{
+    public class UserModel
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+}
